8d8c7d9532493e1bb57cfabb027cbd44561dcdcd
Schuppel1 <sven.schu.mobil@gmail.com>
2022-04-27 23:51:09 +0200

    added heater and all lightbulb
    still an error
    wait till model hat been loaded.
    call "funLoad()"
    and it works

--- www/js/script.js
+++ www/js/script.js
@@ -1,48 +1,131 @@
-
 const modelViewerColor = document.querySelector("model-viewer#color");
-let lamp1Active = false;
 
-// const light_living_room = document.querySelector('.trolololo');
-// console.log(light_living_room);
+const svgLampNames =  ["#imgLampLivingroom","#imgLampBedroom" ,"#imgLampBathroom" ]
+const svgHeaterNames = ["#imgHeaterLivingroom" , "#imgHeateBedroom", "#imgHeaterBathroom"];
 
-function changeColor(colorString, room){
-    let rgba;
-    let svg_bulb 
-    let modelLampName;
+const modelLampNames = ["lamp.1","lamp.2","lamp.3" ];
+const modelHeaterNames = ["heizung.001","heizung.003", "heizung.002"]; 
+let material;
 
-    if (!colorString || !room) {
+let lampBathroom;
+let lampLivingroom; 
+let lampBedroom; 
+
+let heaterBathroom; 
+let heaterLivingroom; 
+let heaterBedroom; 
+
+function changeColor(colorString, room, device){
+    if (!colorString || !room || !device) {
         return;
-      }
-
+    }
     if(room=="livingroom") {
-        svg_bulb = document.querySelector("#bulbColorLivingRoom");
-        modelLampName = "lamp.1";
-    } else if(room=="bedroom") {
-        svg_bulb = document.querySelector("#bulbColorBedroom");
-        modelLampName = "lamp.2";
+        if(device == "heater") {
+            heaterLivingroom.switch();
+        } else if (device == "lamp") {
+            lampLivingroom.switch();
+        } else {
+        console.log("Angegebene Device nicht gefunden")
+            return;
+        }
+    } else if(room=="bedroom") { 
+        if(device == "heater") {
+            heaterBedroom.switch();
+        } else if (device == "lamp") {
+            lampBedroom.switch();
+        } else {
+        console.log("Angegebene Device nicht gefunden")
+            return;
+        }
+    } else if(room=="bathroom") {
+        if(device == "heater") {
+            heaterBathroom.switch();
+        } else if (device == "lamp") {
+            // console.log("buttonpressed")
+            lampBathroom.switch();
+        } else {
+        console.log("Angegebene Device nicht gefunden")
+            return;
+        }
     } else {
-        svg_bulb = document.querySelector("#bulbColorBathroom");
-        modelLampName = "lamp.3"
+        console.log("Angegebene Raum wurde nicht gefunden")
+        return;
     }
+}
 
-    if (!lamp1Active) {
-        //Lampe wird eingeschalten
-        rgba = [0.75, 0.75, 0, 1.0];
-        lamp1Active = true;
-        svg_bulb.style.fill = "#ff0";
+
+
+
+
+
+
+
+window.onload = function funLoad() { 
+    console.log('The Script will load now.'); 
+}
+function funLoad() {
+
+    material = modelViewerColor.model.materials;
+    function SmartHomeDevice(imgID, modelID) {
+
+        let active = false;
+        let imgId = imgID;
+        let modelId = modelID;
+        let htmlButtonElement = document.querySelector(imgID); 
         
-    } else {
-        rgba = [92/255, 84/255, 84/255, 1.0];
-        lamp1Active = false;
-        svg_bulb.style.fill = "#000";
+        this.turnOn = function() {
+            if(!active) {
+    
+                active = true;
+                rgba = [0.75, 0.75, 0, 1.0]
+                // Model wird angeschaltet
+                material.forEach(element => {
+                if(element.name==modelId) {
+                        element.pbrMetallicRoughness.setBaseColorFactor(rgba);
+                        element.setEmissiveFactor(rgba);
+                    }
+                }); 
+                
+                // Button wird angeschalter
+                htmlButtonElement.style.backgroundColor="yellow"; 
+            }
+        }
+    
+        this.turnOff = function() {
+            if(active) {
+                active = false;
+                rgba = [92/255, 84/255, 84/255, 1.0];
+                // Model wird ausgeschalten
+                
+                material.forEach(element => {
+                if(element.name==modelID) {
+                        element.pbrMetallicRoughness.setBaseColorFactor(rgba);
+                        element.setEmissiveFactor(rgba);
+                    }
+                }); 
+                
+                // Button wird angeschalter
+                htmlButtonElement.style.backgroundColor="white"; 
+            }
+        }
+    
+        this.switch = function() {
+            if(active)
+                this.turnOff();
+            else 
+                this.turnOn();
+        }
+        
     }
 
-    const material = modelViewerColor.model.materials;
 
-    material.forEach(element => {
-        if(element.name==modelLampName) {
-            element.pbrMetallicRoughness.setBaseColorFactor(rgba);
-            element.setEmissiveFactor(rgba);
-        };
-    });  
-  };
+
+
+    lampBathroom = new SmartHomeDevice("#imgLampBathroom", "lamp.3" ); 
+    lampLivingroom = new SmartHomeDevice("#imgLampLivingroom", "lamp.1" ); 
+    lampBedroom = new SmartHomeDevice("#imgLampBedroom", "lamp.2" ); 
+
+    heaterBathroom = new SmartHomeDevice("#imgHeaterBathroom", "heizung.002" ); 
+    heaterLivingroom = new SmartHomeDevice("#imgHeaterLivingroom", "heizung.001" ); 
+    heaterBedroom = new SmartHomeDevice("#imgHeaterBedroom", "heizung.001" ); 
+} 
