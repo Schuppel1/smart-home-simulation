4749e2cc9263ad12c6e704d4cfc6e38d03768372
Paul Eitenbichler <paul.eitenbichler@ueberbit.de>
2022-05-20 07:52:15 +0200

    bugfixes

--- www/js/script.js
+++ www/js/script.js
@@ -572,6 +572,9 @@ function changeTime() {
     setDate();
 }
 
+ma_button = document.getElementById("ma_button")
+
+
 //Button-toggle
 function toggleRooms() {
     funLoad();
@@ -581,12 +584,15 @@ function toggleRooms() {
     }
     initializeDeviceSelection()
     if (flag == 0) {
+        console.log(ma_button);
         document.getElementById("rooms").style.display = "none";
         document.getElementById("auto").style.display = "block";
+        ma_button.textContent = 'Manuell';
         flag = 1;
     } else {
         document.getElementById("auto").style.display = "none";
         document.getElementById("rooms").style.display = "block";
+        ma_button.textContent = 'Automatisiert';
         flag = 0;
     }
 }
